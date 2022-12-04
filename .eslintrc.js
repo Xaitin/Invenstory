cf5b4ad121305f55bfbebb2774d605b5ddaf2f22
Xaitin <46392550+Xaitin@users.noreply.github.com>
2022-12-04 00:05:53 -0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "node": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
